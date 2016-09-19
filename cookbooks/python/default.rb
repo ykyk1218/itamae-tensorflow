570b9039eec4c8116cad34ebf7bcda0b3415e145
YoshikiKobayashi <attack_no22001@yahoo.co.jp>
2016-09-19 17:59:55 +0900

    スクレイピング用にlxmlをインストール

--- cookbooks/python/default.rb
+++ cookbooks/python/default.rb
@@ -1,3 +1,12 @@
+package 'libxml2-dev' do
+  action :install
+end
+package 'libxslt1-dev' do
+  action :install
+end
+package 'zlib1g-dev' do
+  action :install
+end
 package 'python3-pip' do
   action :install
 end
@@ -6,5 +15,11 @@ execute "install numpy" do
   user "vagrant"
   command <<-"EOH"
     sudo pip3 install numpy
+  EOH
+end
+execute "install lxml" do
+  user "vagrant"
+  command <<-"EOH"
+    sudo pip3 install lxml
   EOH
 end
